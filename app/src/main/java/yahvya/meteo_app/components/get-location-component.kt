05815f2b2d42d57da8d481191eb0d95181ab8bbb
Yahaya <developpeur.yb@gmail.com>
2024-12-15 16:31:24 +0100

    add location get in location get component (add permissions requirements ...)

--- app/src/main/java/yahvya/meteo_app/components/get-location-component.kt
+++ app/src/main/java/yahvya/meteo_app/components/get-location-component.kt
@@ -1,6 +1,13 @@
 package yahvya.meteo_app.components
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
@@ -8,16 +15,48 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
+/**
+ * @brief component to get user location
+ * @param modifier modifier
+ * @param context location context
+ * @param onLocationGet on location get callback
+ * @param onDeny on location permission deny callback
+ */
 @Composable
 fun GetLocationComponent(
     modifier: Modifier,
-    onButtonClicked: () -> Unit
+    onLocationGet:(Location?) -> Unit,
+    onDeny: () -> Unit,
+    context: Context = LocalContext.current,
 ){
-    Button(modifier = modifier, onClick = onButtonClicked) {
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted)
+            getLastKnownLocation(fusedLocationClient) { location -> onLocationGet(location)}
+        else
+            onDeny()
+    }
+
+    // internal function to ask permission
+    fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            getLastKnownLocation(fusedLocationClient) {location -> onLocationGet(location)}
+        else
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    Button(modifier = modifier, onClick = {requestLocationPermission()}) {
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -27,13 +66,35 @@ fun GetLocationComponent(
     }
 }
 
+/**
+ * @brief provide the last known location
+ * @param fusedLocationClient location provider
+ * @param onLocationReceived location received callback
+ */
+@SuppressLint("MissingPermission")
+private fun getLastKnownLocation(
+    fusedLocationClient: FusedLocationProviderClient,
+    onLocationReceived: (Location?) -> Unit
+) {
+    fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+        onLocationReceived(location)
+    }.addOnFailureListener {
+        onLocationReceived(null)
+    }
+}
+
 @Composable
 @Preview
 fun GetLocationComponentPreview(){
     GetLocationComponent(
         modifier = Modifier,
-        onButtonClicked = {
-            Log.d("Info","Button clicked")
+        onLocationGet = {location ->
+            if(location != null){
+                Log.d("info","Longitude : ${location.longitude} - Latitude : ${location.latitude}")
+            }
+        },
+        onDeny = {
+            Log.d("info","Permission refusée")
         }
     )
 }
